55c118df48e3c2daf1d767ceab791f25394d5d95
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-03-02 12:39:58 +0530

    Added employee attendence

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeWage
+    {
+        const int IS_PRESENT = 1;
+        const int IS_ABSENT = 0;
+        public void GetAttendance()
+        {
+            Random random = new Random();
+            int checkAttendance = random.Next(0, 2);
+            if (checkAttendance == IS_PRESENT)
+                Console.WriteLine("Is Present");
+            else if (checkAttendance == IS_ABSENT)
+                Console.WriteLine("Is Absent");
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeWage employee = new EmployeeWage();
+            employee.GetAttendance();
+            Console.ReadLine();
+        }
+    }
+}
